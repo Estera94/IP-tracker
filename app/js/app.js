4b4270e3f71789d6231006b3b17394d644798fd3
Estera <estera.maftei94@gmail.com>
2021-06-27 14:43:31 +0100

    map reseted

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,10 @@
-var iconImg = L.icon({
+let iconImg = L.icon({
     iconUrl: 'icon.png',
     iconSize: [60, 75],
     iconAnchor: [25, 16]
 });
 
-var mymap = L.map('issMap').setView([51.505, -0.09], 13);
-var marker = L.marker([51.5, -0.09], {icon: iconImg}).addTo(mymap);
+let mymap = L.map('issMap').setView([51.505, -0.09], 3);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -17,18 +16,19 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(mymap);
 
 async function getApiUrl(ipAddress) {
-    let response = await fetch(`https://geo.ipify.org/api/v1?apiKey=at_tdu908rqI4OBlxCEVWNgbWcnMLbnt&ipAddress=${ipAddress}`)
+    let response = await fetch(`https://geo.ipify.org/api/v1?apiKey=at_N7bI2kkvX3c6QIv1J3m6dgvIbJugA&ipAddress=${ipAddress}`)
     let data = await response.json()
     return data
 }
 
 async function injectingHtml() {
+    let marker = L.marker([0, 0], {icon: iconImg}).addTo(mymap);
     let input = document.querySelector(".form-control").value;
     let data = await getApiUrl(input)
     let latitude =  data.location.lat
     let longitude =  data.location.lng
     marker.setLatLng([latitude, longitude])
-    mymap.setView([latitude, longitude])
+    mymap.setView([latitude, longitude], 13)
     const source = document.querySelector('#dataTemplate').innerHTML
     const template = Handlebars.compile(source)
     const temp = document.querySelector('#temp')
